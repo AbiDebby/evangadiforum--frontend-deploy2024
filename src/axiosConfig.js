2dbaa900bef1d260c9f45245a76fdd8968df8c49
mintesnot <mintesnottesfaye@gmail.com>
2024-10-27 23:01:03 +0300

    C:/Program Files/Git/api added after the deployed axios url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://deploy-evagadiforum-2024.onrender.com",
+  baseURL: "https://deploy-evagadiforum-2024.onrender.com/api",
 });
 
 export default axiosBase;
